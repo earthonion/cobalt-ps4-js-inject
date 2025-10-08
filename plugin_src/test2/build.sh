788c5f3b052719139ada31d679426f10283c4996
earthonion <garlic@duck.com>
2025-10-08 02:33:47 -0400

    Remove nested plugin_src/test2 directory

--- plugin_src/test2/build.sh
+++
@@ -1,5 +0,0 @@
-make clean
-make
-echo "BUILD SUCCESS!"
-curl -T ../../bin/plugins/prx_final/test2.prx ftp://192.168.0.106:2121/data/GoldHEN/plugins/test2.prx
-curl -T inject.js ftp://192.168.0.106:2121/data/inject.js
